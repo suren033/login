25da139105defd68dda01b3c9b5d9fdbdf01224b
Hayko <hayk-martirosyan-2001@mail.ru>
2019-11-25 17:32:12 +0400

    login

---
+++ loginn/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="check.php" method="POST">
+		<input type="text" name="username" placeholder="name"><br><br>
+		<input type="password" name="password" placeholder="password"><br><br>
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
